60e3911eb26329493d9a1c85a412ec31888977e0
José Eduardo García Beltrán <eduardo@tolk.mx>
2024-07-01 00:22:04 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/app/WebServer.java
@@ -0,0 +1,197 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Michael Pogrebinsky - Distributed Systems & Cloud Computing with Java
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package com.mycompany.app;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+public class WebServer {
+
+    private static final String STATUS_ENDPOINT = "/status";
+    private static final String HOME_PAGE_ENDPOINT = "/";
+    private static final String HOME_PAGE_UI_ASSETS_BASE_DIR = "/ui_assets/";
+    private static final String ENDPOINT_PROCESS = "/procesar_datos";
+    private static final String CENTRAL_SERVER_SEARCH_ENDPOINT = "http://localhost:8080/central/search?n=2";
+
+    private final int port;
+    private HttpServer server;
+    private final ObjectMapper objectMapper;
+    private final HttpClient httpClient;
+
+    public WebServer(int port) {
+        this.port = port;
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public void startServer() {
+        try {
+            this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
+        HttpContext taskContext = server.createContext(ENDPOINT_PROCESS);
+        HttpContext homePageContext = server.createContext(HOME_PAGE_ENDPOINT);
+        statusContext.setHandler(this::handleStatusCheckRequest);
+        taskContext.setHandler(this::handleTaskRequest);
+        homePageContext.setHandler(this::handleRequestForAsset);
+
+        server.setExecutor(Executors.newFixedThreadPool(8));
+        server.start();
+    }
+
+    private void handleRequestForAsset(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            exchange.close();
+            return;
+        }
+
+        byte[] response;
+
+        String asset = exchange.getRequestURI().getPath();
+
+        if (asset.equals(HOME_PAGE_ENDPOINT)) {
+            response = readUiAsset(HOME_PAGE_UI_ASSETS_BASE_DIR + "index.html");
+        } else {
+            response = readUiAsset(asset);
+        }
+        addContentType(asset, exchange);
+        sendResponse(response, exchange);
+    }
+
+    private byte[] readUiAsset(String asset) throws IOException {
+        InputStream assetStream = getClass().getResourceAsStream(asset);
+
+        if (assetStream == null) {
+            return new byte[] {};
+        }
+        return assetStream.readAllBytes();
+    }
+
+    private static void addContentType(String asset, HttpExchange exchange) {
+
+        String contentType = "text/html";
+        if (asset.endsWith("js")) {
+            contentType = "text/javascript";
+        } else if (asset.endsWith("css")) {
+            contentType = "text/css";
+        }
+        exchange.getResponseHeaders().add("Content-Type", contentType);
+    }
+
+    private void handleTaskRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+
+        try {
+            FrontendSearchRequest frontendSearchRequest = objectMapper
+                    .readValue(exchange.getRequestBody().readAllBytes(), FrontendSearchRequest.class);
+
+            // Realiza la solicitud al servidor central
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(CENTRAL_SERVER_SEARCH_ENDPOINT))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Procesa la respuesta del servidor central
+            byte[] responseBytes = response.body().getBytes();
+            sendResponse(responseBytes, exchange);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            exchange.close();
+            return;
+        }
+
+        String responseMessage = "El servidor está vivo\n";
+
+        Map<String, Object> status = new HashMap<>();
+        status.put("cpuUsage", getCpuUsage());
+        status.put("message", "El servidor está en línea y funcionando.");
+        status.put("status", "online");
+
+        sendResponse(responseMessage.getBytes(), status, exchange);
+    }
+
+    // Método para obtener el uso de la CPU
+    public double getCpuUsage() {
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+        // Esto es una aproximación, puedes ajustar según lo que necesites
+        return osBean.getSystemLoadAverage();
+    }
+
+    private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private void sendResponse(byte[] responseBytes, Map<String, Object> status, HttpExchange exchange)
+            throws IOException {
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        sendResponse(responseBytes, exchange);
+    }
+
+    // public static void main(String[] args) {
+    // WebServer webServer = new WebServer(8080);
+    // webServer.startServer();
+    // }
+}
